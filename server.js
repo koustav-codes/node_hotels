46ba245d388a333bf29bdf15f44c705d53be666a
koustavbiswas-ui <koustavbiswas6726@gmail.com>
2026-02-28 01:18:10 +0530

    third phase of hotel project

--- server.js
+++ server.js
@@ -3,15 +3,26 @@ const app = express();
 const db = require('./db');
 const Person = require('./person');
 const Menu = require('./menuItems');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy; // THIS IS ALSO CALLED USER-NAME AND PASSWORD STRATEGY
 require('dotenv').config();
 
 app.use(express.json()); // IMPORTANT
+
+const logRequest = (req, res, next) => {
+  console.log(`${new Date().toLocaleString()} Request Made to : ${req.originalUrl}`);
+  next(); // Move on to the next phase
+};
+
+app.use(logRequest);
 
 app.get('/', (req, res) => {
   res.send('Hello Everyone this is Koustav');
 });
 
 const port = process.env.PORT || 3020
+
+
 
 const personRoutes = require('./personRoutes');
 app.use('/person', personRoutes);
